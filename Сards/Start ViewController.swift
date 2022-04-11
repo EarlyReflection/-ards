7b96a7d522fb5fe7e73447eed7d1a7cbd748baa4
Vladimir Dvornikov <echobizarre@gmail.com>
2022-04-11 19:51:01 +0300

    Fixed text field

--- Сards/Start ViewController.swift
+++ Сards/Start ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var phoneNumberTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
